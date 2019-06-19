d803a78e1d2d7b5b9aa164b3089dc77cd736217b
AlMak47 <almamy@smartechguinee.com>
2019-06-19 15:54:53 +0000

    initialisation

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Activite github</title>
+</head>
+
+<body>
+
+	<form>
+		<h2>Formulaire de Contact</h2>
+		<input type="text" name="Nom">
+		<input type="submit" name="" value="Envoyer">
+	</form>
+
+
+<script type="text/javascript" src="script.js"></script>
+</body>
+</html>
